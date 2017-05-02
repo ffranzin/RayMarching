838f8fc06991d77309e56be467f31a273e0d2485
ffranzin <ffranzin@inf.ufsm.br>
2017-05-01 21:12:11 -0300

    first commit

---
+++ Assets/RayMarchParameters.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(Camera))]
+[AddComponentMenu("Effects/RayMarch")]
+public class RayMarchParameters : SceneViewFilter
+{
+
+    [SerializeField]
+    private Shader _EffectShader;
+    public Transform SunLight;
+
+
+    void Start()
+    {
+        CurrentCamera.depthTextureMode = DepthTextureMode.Depth;
+    }
+
+
+    public Material EffectMaterial
+    {
+        get
+        {
+            if (!_EffectMaterial && _EffectShader)
+            {
+                _EffectMaterial = new Material(_EffectShader);
+                _EffectMaterial.hideFlags = HideFlags.HideAndDontSave;
+            }
+            return _EffectMaterial;
+        }
+    }
+    private Material _EffectMaterial;
+
+
+    public Camera CurrentCamera
+    {
+        get
+        {
+            if (!_CurrentCamera)
+            {
+                _CurrentCamera = GetComponent<Camera>();
+            }
+            return _CurrentCamera;
+        }
+    }
+    private Camera _CurrentCamera;
+
+
+
+    //[ImageEffectOpaque]
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (!EffectMaterial)
+        {
+            Graphics.Blit(source, destination);
+            return;
+        }
+
+        EffectMaterial.SetMatrix("_FrustumCornerES", GetFrustumCorners(CurrentCamera));
+        EffectMaterial.SetMatrix("_CameraInvViewMatrix", CurrentCamera.cameraToWorldMatrix);
+        EffectMaterial.SetVector("_cameraWS", CurrentCamera.transform.position);
+        EffectMaterial.SetVector("_LightDir", SunLight ? SunLight.forward : Vector3.down);
+        CustomGraphicsBlit(source, destination, EffectMaterial, 0);
+        //Graphics.Blit(source, destination, EffectMaterial, 0);
+    }
+
+    static void CustomGraphicsBlit(RenderTexture source, RenderTexture dest, Material fxMaterial, int nPass)
+    {
+        RenderTexture.active = dest;
+        fxMaterial.SetTexture("_MainTex", source);
+
+        GL.PushMatrix();
+        GL.LoadOrtho();
+
+        fxMaterial.SetPass(nPass);
+
+        GL.Begin(GL.QUADS);
+
+        GL.MultiTexCoord2(0, 0.0f, 0.0f);
+        GL.Vertex3(0.0f, 0.0f, 3.0f); // BL
+
+        GL.MultiTexCoord2(0, 1.0f, 0.0f);
+        GL.Vertex3(1.0f, 0.0f, 2.0f); // BR
+
+        GL.MultiTexCoord2(0, 1.0f, 1.0f);
+        GL.Vertex3(1.0f, 1.0f, 1.0f); // TR
+
+        GL.MultiTexCoord2(0, 0.0f, 1.0f);
+        GL.Vertex3(0.0f, 1.0f, 0.0f); // TL
+
+        GL.End();
+
+        GL.PopMatrix();
+    }
+
+    private Matrix4x4 GetFrustumCorners(Camera cam)
+    {
+        float camFov = cam.fieldOfView;
+        float camAspect = cam.aspect;
+        float fovWHalf = camFov * 0.5f;
+        float tanFov = Mathf.Tan(fovWHalf * Mathf.Deg2Rad);
+        Matrix4x4 frustumCorners = Matrix4x4.identity;
+
+        Vector3 toRight = Vector3.right * tanFov * camAspect;
+        Vector3 toTop = Vector3.up * tanFov;
+
+        Vector3 topLeft = (-Vector3.forward - toRight + toTop);
+        Vector3 topRight = (-Vector3.forward + toRight + toTop);
+        Vector3 bottomRight = (-Vector3.forward + toRight - toTop);
+        Vector3 bottomLeft = (-Vector3.forward - toRight - toTop);
+
+        frustumCorners.SetRow(0, topLeft);
+        frustumCorners.SetRow(1, topRight);
+        frustumCorners.SetRow(2, bottomRight);
+        frustumCorners.SetRow(3, bottomLeft);
+
+        //Debug.DrawRay(cam.transform.position, topLeft * 100, Color.red);
+        //Debug.DrawRay(cam.transform.position, topRight * 100, Color.red);
+        //Debug.DrawRay(cam.transform.position, bottomLeft * 100, Color.red);
+        //Debug.DrawRay(cam.transform.position, bottomRight * 100, Color.red);
+
+        return frustumCorners;
+    }
+
+}
---
+++ Assets/RayMarchShader.shader
@@ -0,0 +1,142 @@
+﻿// Upgrade NOTE: replaced 'mul(UNITY_MATRIX_MVP,*)' with 'UnityObjectToClipPos(*)'
+
+Shader "Effects/RayMarchShader"
+{
+	Properties
+	{
+		_Color("Color", Color) = (1,1,1,1)
+		_MainTex ("Texture", 2D) = "white" {}
+	}
+	SubShader
+	{
+		Cull Off ZWrite Off ZTest Always
+		Pass
+		{
+			Tags { "RenderType"="Opaque" }
+			CGPROGRAM
+	
+			#pragma vertex vert
+			#pragma fragment frag
+			#pragma multi_compile __ DEBUG_PERFORMANCE
+			#pragma target 3.0
+
+			#include "UnityCG.cginc"
+
+			uniform float4x4 _FrustumCornerES;
+			uniform float4x4 _CameraInvViewMatrix;
+			uniform float4 _MainTex_TexelSize;
+			sampler2D _MainTex;
+			uniform float3 _LightDir;
+			uniform float3 _cameraWS;
+			fixed4 colorTest = fixed4(1,0,0,0);
+
+			fixed4 _Color;
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				float4 pos : SV_POSITION;
+				float3 ray : TEXCOORD1;
+			};
+
+			float sdSphere( float3 p, float s )
+			{
+			  return length(p) - s;
+			}
+
+			float sdTorus(float3 p, float2 t)
+			{
+				float2 q = float2(length(p.xz) - t.x, p.y);
+				return length(q) - t.y;
+			}
+
+			
+			float map(float3 p) {
+				return sdSphere(p, 1);
+				return sdTorus(p, float2(1, 0.2));
+			}
+
+			float3 CalcNormal(float3 pos)
+			{
+				const float2 eps = float2(0.001, 0.0);
+			
+				float3 normal = float3(
+								map(pos + eps.xyy).x - map(pos - eps.xyy).x,
+								map(pos + eps.yxy).x - map(pos - eps.yxy).x,
+								map(pos + eps.yyx).x - map(pos - eps.yyx).x);
+
+				return normalize(normal);
+			}
+
+
+			//Parameters: ro = rayOrigin rd = rayDirection
+			fixed4 RayMarching( float3 ro, float3 rd)
+			{
+				fixed4 ret = fixed4(1,0,0,0);
+
+				const int maxStep = 128;
+				float dist = 0;
+
+				for(int i=0; i<maxStep; i = i+1)
+				{
+					float3 p = ro + rd * dist;
+
+					float d = map(p);
+
+					if(d < 0.001)
+					{
+						float nor = CalcNormal(p);
+						ret = fixed4(dot(- _LightDir.xyz, nor).rrr, 1);
+						break;
+					}
+
+					dist += d;
+				}
+				return ret;
+			}
+
+
+
+			v2f vert (appdata IN)
+			{
+				v2f OUT;
+
+				half index = IN.vertex.z; // index passed via custom blit function
+				IN.vertex.z = 0.1;
+
+				OUT.pos = UnityObjectToClipPos(IN.vertex);
+				OUT.uv = IN.uv.xy;
+
+				#if UNITY_UV_STARTS_AT_TOP
+					if (_MainTex_TexelSize.y < 0)
+						OUT.uv.y = 1 - OUT.uv.y;
+				#endif		
+				
+				OUT.ray = _FrustumCornerES[(int)index].xyz;
+				OUT.ray = mul(_CameraInvViewMatrix, OUT.ray);
+
+				return OUT;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target
+			{	
+				float3 ro = _cameraWS;
+				float3 rd = normalize(i.ray.xyz);
+
+				fixed3 col = tex2D(_MainTex, i.uv);
+				fixed4 add = RayMarching(ro, rd);
+				
+				return fixed4(col*(1.0 - add.w) + add.xyz * add.w, 1.0);
+			}
+		
+			ENDCG
+		}
+
+	}
+}
